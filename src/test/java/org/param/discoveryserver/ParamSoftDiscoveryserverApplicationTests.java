3b07c434028555a8a6f3693e4032b706f45cc4dc
param-sde <parameshwaran.pv@gmail.com>
2025-05-24 00:03:30 +0530

    Initial Commit for Discovery Server

---
+++ src/test/java/org/param/discoveryserver/ParamSoftDiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.param.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamSoftDiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
